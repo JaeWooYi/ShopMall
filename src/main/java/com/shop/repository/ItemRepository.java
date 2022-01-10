f2fb902e5dc1de2a566c6f13695e19ee5280fbcf
YiJaeWoo <wodn0101@happytoseeyou.co.kr>
2022-01-10 09:24:45 +0900

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByItemNm(String itemNm, String itemDetail);
+    List<Item> findByItemOrItemDetail(String 테스트_상품1, String 테스트_상품_상세_설명5);
     List<Item> findByPriceLessThan(Integer price);
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
-
 }
--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -60,6 +60,16 @@ class ItemRepositoryTest {
     }
 
     @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
     @DisplayName("가격 LessThan 테스트")
     public void findByPriceLessThanTest(){
         this.createItemList();
