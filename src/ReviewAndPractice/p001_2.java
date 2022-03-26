1637a82084c2ba3dc1bd63c0117d3d3c02340583
JaeWooYi <wodn0101@gmail.com>
2022-03-26 15:16:19 +0900

    1st

--- src/ReviewAndPractice/p001_2.java
+++ src/ReviewAndPractice/p001_2.java
@@ -3,7 +3,7 @@ package ReviewAndPractice;
 public class p001_2 {
 	
 	public static void main(String[] args) {
-		
+		// 1. 
 		int d = justSum(100,200);
 		System.out.println(d);
 		
@@ -11,12 +11,22 @@ public class p001_2 {
 		System.out.println("---------------");
 		System.out.println();
 		
+		// 2. 
 		int[] summary = arrSum();
 		int hap = 0;
 		for(int i = 0; i < summary.length; i++) {
 			hap += summary[i];
 		}
 		System.out.println(hap);
+		
+		System.out.println();
+		System.out.println("---------------");
+		System.out.println();
+		
+		//3. 
+		int[] kkk = new int[] {2,4,6,8,10};
+		int result = sumThird(kkk);
+		System.out.println(result);
 		
 	}
 	// 1.
@@ -35,6 +45,15 @@ public class p001_2 {
 		arrEx[4] = 9;
 		
 		return arrEx;
+	}
+	
+	//3.
+	public static int sumThird(int[] anything) {
+		int hap2 = 0;
+		for(int i = 0; i < anything.length; i++) {
+			hap2 += anything[i];
+		}
+		return hap2;
 	}
 
 }
