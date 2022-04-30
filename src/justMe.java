cf4885967da6ba2720470ac3bbd981859c2b12ee
JaeWooYi <wodn0101@gmail.com>
2022-04-30 14:52:54 +0900

    Why?

---
+++ src/justMe.java
@@ -0,0 +1,12 @@
+
+public class justMe {
+	public static void main(String[] args) {
+		for (int i = 10; i > 0; --i) {
+			System.out.println(i);
+		}
+		
+//		for (int k = 0; k < 10; k++) {
+//			System.out.println(k);
+//		}
+	}
+}
