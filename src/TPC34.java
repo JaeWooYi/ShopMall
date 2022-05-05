ba1b12d11ca76bbdd5f67beb77d7c9ed73834693
JaeWooYi <wodn0101@gmail.com>
2022-05-05 16:21:36 +0900

    Child Day

---
+++ src/TPC34.java
@@ -0,0 +1,15 @@
+
+public class TPC34 {
+	public static void main(String[] args) {
+		// String
+		java.lang.String str = new java.lang.String("APPLE");	// java.lang 생략되있는거야 -> 임포트 역시
+		String v = str.toLowerCase();
+		System.out.println(v);
+		System.out.println(str.charAt(4));
+		System.out.println(str.length());
+		System.out.println(str.indexOf("PL"));
+		System.out.println(str.indexOf("PS"));	// -1 : 없다는 뜻
+		System.out.println(str.replace("E", "e"));
+		System.out.println();
+	}
+}
